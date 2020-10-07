dbe91500ecc77fa2bb75746ca6c6a6dc4cc234d7
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-07 16:09:55 -0500

    add static template to dashboard and import word data

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import LanguageContext from '../../contexts/LanguageContext';
+import WordData from '../../components/WordData/WordData'
 
 class DashboardRoute extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        isExpanded: true,
+    };
+}
   static contextType = LanguageContext
   render() {
     console.log(this.context.language)
+
+    const toggleLanguageComponent = () => {
+      if (this.state.isExpanded === true){
+        return (
+          <div className="word-data table">
+            <WordData/>          
+          </div>          
+        )
+      }
+    }
+
     return (
-      <section>
-        implement and style me
+      <section className="language-component">
+        <div className="language-header">
+          <h3>Latin</h3>          
+        </div>
+
+        {toggleLanguageComponent()}
+
+        <p className="total-score">Total Score:</p>
+
+        <button className="start-learning-button">
+          START PRACTICING
+        </button>
       </section>
     );
   }
