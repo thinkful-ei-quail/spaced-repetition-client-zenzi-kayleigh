ff605d349209def9bf5175e68cb5037ea63ee64c
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-07 17:54:52 -0500

    revert token key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT: 'https://still-savannah-14808.herokuapp.com/api',
   //API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'token-key',
+  TOKEN_KEY: '0e9fe658-17da-4642-ab59-025408e06105',
 }
