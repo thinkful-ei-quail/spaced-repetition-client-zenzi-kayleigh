f089eca38372ff9df49cb337d7a412062a2431ab
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-06 21:54:42 -0500

    center summary

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import React, { Component } from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './RegistrationRoute.css';
 
 class RegistrationRoute extends Component {
   static defaultProps = {
@@ -16,16 +17,18 @@ class RegistrationRoute extends Component {
   render() {
     return (
       <section>
-        <p>
-          Practice learning a language with the spaced reptition revision technique.
-        </p>
-        <div className="registration-component">
+        <div className="registration-component right">
+          <p className="summary">
+            Practice learning a language with the spaced reptition revision technique.
+          </p>
+        </div>
+
+        <div className="registration-component left">
           <h2>Sign up</h2>
           <RegistrationForm
             onRegistrationSuccess={this.handleRegistrationSuccess}
           />
         </div>
-
       </section>
     );
   }
