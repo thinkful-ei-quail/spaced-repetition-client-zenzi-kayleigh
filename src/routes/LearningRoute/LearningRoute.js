bd98093846ff9c533e9e54dfdfa5a908eb7b0e35
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-08 02:31:10 -0500

    update routes for response

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -10,24 +10,25 @@ class LearningRoute extends Component {
     };
   }
   static contextType = LanguageContext;
-  
-  render() {
-    console.log(this.state.input_result)
-    const updateResultOnSubmit = (input) =>{
-      if (input === this.context.original){
-        this.setState({input_result: true})
-      }
-      this.setState({input_result: false})
-    };
-    const resetResultOnNext = () =>{
-      this.setState({input_result: null})
+  updateInput = (input) =>{
+    this.setState({input: {input}})
+  }
+  updateResultOnSubmit = () =>{
+    if (this.state.input === this.context.original){
+      this.setState({input_result: true})
     }
+    this.setState({input_result: false})
+  };
+  resetResultOnNext = () =>{
+    this.setState({input_result: null})
+  }
+  render() {
     const renderPage = () =>{
-      if(this.context.head === null){
-        return (
-          <h2>Content Loading...</h2>
-        )
-      }
+      // if(this.context.head === null){
+      //   return (
+      //     <h2>Content Loading...</h2>
+      //   )
+      // }
       if (this.state.input_result === null){
         return (
           <section>
@@ -36,22 +37,66 @@ class LearningRoute extends Component {
               <label>
                 What's the translation for this word?
               </label>
-              <input type="text"/>
+              <input 
+                type='text' 
+                id='learn-guess-input'
+                onChange={(e) => this.updateInput(e.target.value)}
+              />
+              <button
+                type='submit'
+              >
+                Submit
+              </button>
             </form>
+            <div className='inactive-score-container'>
+              <p>Previous Tries:</p>
+              <p>0 Correct / 2 Incorrect</p>
+              <p>Score Average: 0%</p>
+            </div>
           </section>     
         )
       }
       if (this.state.input_result === true){
         return (
           <section>
-            correct answer page
-          </section>
+            <h2>You were correct: {`:D`}</h2>
+            <div className='response'>
+              <h4>The correct translation for</h4>
+              <p>Salve</p>
+              <h4>was</h4>
+              <p>Hello</p>
+              <h4>You chose</h4>
+              <p>Hello</p>              
+            </div>
+            <div className='active-score-container'>
+              <p>Your Score Is Now:</p>
+              <p>20%</p>
+            </div>
+            <button onClick={()=>this.resetResultOnNext()}>
+              Try another word!
+            </button>
+          </section>  
         )
       }
       if (this.state.input_result === false){
         return (
           <section>
-            incorrect answer page
+            <h2>Good try, but not quite right {`:(`}</h2>
+            <div className='response'>
+              <h4>The correct translation for</h4>
+              <p>Salve</p>
+              <h4>was</h4>
+              <p>Hello</p>
+              <h4>You chose</h4>
+              <p>Night</p>              
+            </div>
+            <div className='active-score-container'>
+              <p>Your Score Is Now:</p>
+              <p>0%</p>
+            </div>
+            <button onClick={()=>this.resetResultOnNext()}>
+              Try another word!
+            </button>
           </section>
         )
       }else{
