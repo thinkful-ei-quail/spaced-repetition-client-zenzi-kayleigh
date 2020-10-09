5657f3af8131320e50c954eef415aabaedc1ab83
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-09 12:15:42 -0500

    learning route reformat for tests

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react'
+import config from '../../config'
 import LanguageContext from '../../contexts/LanguageContext'
 
 class LearningRoute extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: '',
-      input_result: null,
+      guess: '',
+      is_correct: null,
     };
   }
   static contextType = LanguageContext;
-  updateInput = (input) =>{
-    this.setState({input: {input}})
+  updateGuess = (guess) =>{
+    this.setState({guess: {guess}})
   }
+  submitGuess = (e) => {
+    e.preventDefault();
+    fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        guess: `${this.state.guess.value}`,
+      }),
+    })
+    .then((res) => {
+      return res.json();
+    });
+    // .then((response) => {
+    //   this.context.addGuess();
+    //   this.setState({
+    //     redirect: "/learn",
+    //   });
+    // })
+  };
   updateResultOnSubmit = () =>{
-    if (this.state.input === this.context.original){
-      this.setState({input_result: true})
+    if (this.state.guess === 'test'){
+      this.setState({is_correct: true})
     }else{
-      this.setState({input_result: false})
+      this.setState({is_correct: false})
     }
   };
   resetResultOnNext = () =>{
-    this.setState({input_result: null})
+    this.setState({is_correct: null})
   }
   render() {
     const renderPage = () =>{
@@ -30,34 +52,37 @@ class LearningRoute extends Component {
           <h2>Content Loading...</h2>
         )
       }
-      if (this.state.input_result === null){
+      if (this.state.is_correct === null){
+        const { nextWord, wordCorrectCount,wordIncorrectCount, totalScore}= this.context.head
         return (
           <section>
             <h2>Translate the word:</h2>
-            <form onSubmit={ ()=>this.updateResultOnSubmit()}>
-              <label>
+            <span>{nextWord}</span>
+            <form onSubmit={ ()=>this.submitGuess()}>
+              <label htmlFor='learn-guess-input'>
                 What's the translation for this word?
               </label>
               <input 
                 type='text' 
                 id='learn-guess-input'
-                onChange={(e) => this.updateInput(e.target.value)}
+                required
+                onChange={(e) => this.updateGuess(e.target.value)}
               />
               <button
                 type='submit'
               >
-                Submit
+                Submit your answer
               </button>
             </form>
             <div className='inactive-score-container'>
-              <p>Previous Tries:</p>
-              <p>0 Correct / 2 Incorrect</p>
-              <p>Score Average: 0%</p>
+              <p>Your total score is: {totalScore}</p>
+              <p>You have answered this word correctly {wordCorrectCount} times.</p>
+              <p>You have answered this word incorrectly {wordIncorrectCount} times.</p>
             </div>
           </section>     
         )
       }
-      if (this.state.input_result === true){
+      if (this.state.is_correct === true){
         return (
           <section>
             <h2>You were correct: {`:D`}</h2>
@@ -79,8 +104,8 @@ class LearningRoute extends Component {
           </section>  
         )
       }
-      if (this.state.input_result === false){
-        console.log(this.state.input)
+      if (this.state.is_correct === false){
+        console.log(this.state.guess)
         return (
           <section>
             <h2>Good try, but not quite right {`:(`}</h2>
@@ -96,7 +121,7 @@ class LearningRoute extends Component {
               <p>Your Score Is Now:</p>
               <p>0%</p>
             </div>
-            <button onClick={()=>this.resetResultOnNext()}>
+            <buttonon Click={()=>this.resetResultOnNext()}>
               Try another word!
             </button>
           </section>
